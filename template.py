22d9644cf4b44ee045412622c46725b58b458583
tijekhaled <116840456+tijekhaled@users.noreply.github.com>
2022-10-28 02:14:25 +0700

    Add files via upload

---
+++ template.py
@@ -0,0 +1,15 @@
+# Latihan Soal UTS KU1102 K16
+# Asisten Akademik: Theodore Maximillan Jonathan (13219021)
+
+# Nomor Soal    :
+# NIM           : 
+# Nama          :
+
+# PROGRAM _______________ [isi dengan judul]
+# Tuliskan judul program dan penjelasan singkat.
+
+# KAMUS
+# Isilah dengan definisi variabel yang digunakan.
+
+# ALGORITMA
+# Jangan lupa memberikan komentar secukupnya.
